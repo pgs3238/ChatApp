dd0339c8e52d962469e2e6f03ed19de4934efeca
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2023-05-04 22:03:32 +0200

    Chat App entwicklung Teil 1

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ChatEngine
+          height="100vh"
+          projectID="f8d494d2-1097-4a56-98a4-7fdb896b8a58"
+          userName="Paul-Admin"
+          userSecret="Admin99"
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        />
   );
 }
 
 export default App;
+
+//  https://www.youtube.com/watch?v=jcOKU9f86XE&t=754sS
+
+//  chatengine.io
+//  Login:      paul-gerhard.siegel@hs-osnabrueck.de
+//  Passwort:   NewYork99
+
+//  User 1:
+//  Login:      Paul-Admin
+//  Passwort:   Admin99
+
+//  User 2:
+//  Login:      Mehmet
+//  Passwort:   123123
+
+
